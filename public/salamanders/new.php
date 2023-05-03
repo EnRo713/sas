6708034dcca7f173b5c03863287f0be0ee46af03
EnRo713 <enriquearodriguez@students.abtech.edu>
2023-05-03 16:12:10 -0400

    Edit create.php and new.php

--- public/salamanders/new.php
+++ public/salamanders/new.php
@@ -1,23 +1,24 @@
 <?php 
 require_once('../../private/initialize.php'); 
 
-if (is_post_request()) {
-    $name = $_POST['name'];
-    echo "Salamander Name: $name";
-} else {
-   // redirect_to(url_for('/salamanders/new.php'));
-}
-
 $pageTitle = "Create";
 include (SHARED_PATH . '/salamander-header.php');
 ?>
-<a href= "<?= url_for('/salamander/index.php'); ?>">&laquo; Back to List</a>
+
+<a href= "<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
 <h1>Create Salamander</h1>
-<!-- add label -->
+
 <form action="<?= url_for('/salamanders/create.php'); ?>" method="post">
-    <label for="name">Name</label><br>
-    <input type="text" name="name"/><br>
-    <input type="submit" value="Create Salamander"/>
+  <label for="name">Name</label><br>
+  <input type="text" name="name"/><br>
+
+  <label for="habitat">Habitat</label><br>
+  <textarea id="habitat" name="habitat" rows="6" cols="50"></textarea><br>
+
+  <label for="description">Description</label><br>
+  <textarea id="description" name="description" rows="6" cols="50"></textarea><br>
+
+  <input type="submit" value="Create Salamander">
 </form>
 
 <?php include (SHARED_PATH . '/salamander-footer.php'); ?>
